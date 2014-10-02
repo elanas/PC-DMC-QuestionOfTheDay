f4782a7d41a801c8ea13f966732d2d355fcdbba6
Elana Stroud <estroud1@jhu.edu>
2014-10-02 17:37:27 -0400

    fixing heroku

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+process.env.PWD = process.cwd()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -19,7 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(process.env.PWD + '/htdocs'));
 
 app.use('/', routes);
 app.use('/piechart', routes);
