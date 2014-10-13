28605f8dc246d5a4539e1f63a9ea1b71f342ae39
Elana Stroud <estroud1@jhu.edu>
2014-10-12 23:38:52 -0400

    test

--- public/javascripts/question-page.js
+++ public/javascripts/question-page.js
@@ -51,10 +51,11 @@
             console.log($scope.q);
                 $http.post('/testPost', $scope.q).success(function() {
                     console.log("I am posting!")
+                    $window.location.reload();
+
                 }).error(function() {
                     console.log("I failed to post, caused an error");
                 });
-            $window.location.reload();
 
         }
 
