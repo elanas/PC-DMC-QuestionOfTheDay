edf08eadfa40c1e806fe329872636c80b48ad8b4
Elana Stroud <estroud1@jhu.edu>
2014-10-13 01:21:14 -0400

    test

--- database.js
+++ database.js
@@ -31,7 +31,7 @@ var exportToCSV = function() {
     spawn('.\\bin\\mongoexport', [
         '--db', 'questionDb', '--collection', 'test', '--csv',
         '--fields', 'question,answers',
-        '--out', '..\\test.csv'
+        '--out', '..\\windowstestcsv.csv'
     ]);
 
 
