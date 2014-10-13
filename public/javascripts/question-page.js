6f1818c0a84e5704b004ef23f1c4f01829fe73e8
Elana Stroud <estroud1@jhu.edu>
2014-10-12 23:36:27 -0400

    test

--- public/javascripts/question-page.js
+++ public/javascripts/question-page.js
@@ -49,11 +49,11 @@
             }
             console.log("am i prinitng?");
             console.log($scope.q);
-//                $http.post('/testPost', $scope.q).success(function() {
-//                    console.log("I am posting!")
-//                }).error(function() {
-//                    console.log("I failed to post, caused an error");
-//                });
+                $http.post('/testPost', $scope.q).success(function() {
+                    console.log("I am posting!")
+                }).error(function() {
+                    console.log("I failed to post, caused an error");
+                });
         }
 
         $scope.refresh = function() {
