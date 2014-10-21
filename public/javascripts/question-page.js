867b90ab7f8d3b0183abe96ea06f19b34d5fec4b
Elana Stroud <estroud1@jhu.edu>
2014-10-21 12:41:53 -0400

    extending time of thank you

--- public/javascripts/question-page.js
+++ public/javascripts/question-page.js
@@ -67,7 +67,7 @@ var num = 1;
 //                $scope.startFadeout = false;
 //
 //                $scope.startFadein = true;
-            }, 5000);
+            }, 8000);
 
 //            $scope.startFadein = true;
 //            setTimeout(function() {
